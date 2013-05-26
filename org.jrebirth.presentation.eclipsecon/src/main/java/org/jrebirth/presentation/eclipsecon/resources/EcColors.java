0ab3d395d0f0df7a0c74e60d1d29b424eb64c270
Sebastien Bordes <sebastien.bordes@jrebirth.org>
2013-05-26 21:22:19 +0200

    CopyPaste kills kitten

---
+++ org.jrebirth.presentation.eclipsecon/src/main/java/org/jrebirth/presentation/eclipsecon/resources/EcColors.java
@@ -0,0 +1,64 @@
+/**
+ * Get more info at : www.jrebirth.org .
+ * Copyright JRebirth.org © 2011-2013
+ * Contact : sebastien.bordes@jrebirth.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jrebirth.presentation.eclipsecon.resources;
+
+import javafx.scene.paint.Color;
+
+import org.jrebirth.core.resource.ResourceBuilders;
+import org.jrebirth.core.resource.color.ColorBuilder;
+import org.jrebirth.core.resource.color.ColorItem;
+import org.jrebirth.core.resource.color.ColorParams;
+import org.jrebirth.core.resource.color.WebColor;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The class <strong>EcColors</strong>.
+ * 
+ * @author Sébastien Bordes
+ * 
+ */
+public enum EcColors implements ColorItem {
+
+    /** The slide title. */
+    SLIDE_TITLE(new WebColor("0088D3", 1.0));
+
+    /**
+     * Private Constructor.
+     * 
+     * @param colorParams the primitive values for the color
+     */
+    private EcColors(final ColorParams colorParams) {
+        builder().storeParams(this, colorParams);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Color get() {
+        return builder().get(this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ColorBuilder builder() {
+        return ResourceBuilders.COLOR_BUILDER;
+    }
+}
